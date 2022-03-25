24e101a0a98eec9cd1c92bbf81d8ba0c50635784
AbdelrahmanMohamed <abdelrahman.muhamedd@gmail.com>
2022-03-25 02:25:37 +0200

    status attribute added to track confirmations or declines

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_235633) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_25_002301) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_235633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "friend_id"
+    t.boolean "status", default: false
     t.index ["friend_id"], name: "index_friendlists_on_friend_id"
     t.index ["user_id"], name: "index_friendlists_on_user_id"
   end
