07e29d5d59ecb576d8a85edaeb37b3af267dafd5
AbdelrahmanMohamed <abdelrahman.muhamedd@gmail.com>
2022-03-25 02:35:05 +0200

    requests send by a user added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_picture
+  has_many :friendlists
+  has_many :friends, through: :friendlists
+  has_many :friend_requests_sent, ->{where(status: false)}, class_name: 'Friendlist'
+  has_many :requests_sent, through: :friend_requests_sent, source: :friend
+
+
+
+  # Validations 
+
+
   validates :profile_picture, presence: true, blob: { content_type: :image }
 
 
@@ -12,7 +22,5 @@ class User < ApplicationRecord
   validates_presence_of :phone_number
   validates_uniqueness_of :phone_number
   validates :phone_number, numericality: {message: "must be numeric value" }, length: { is: 7 , message: "must be more than 7 digits" }
-
-
 
 end
