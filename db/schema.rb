41528cfc951d03b4b92e7ea2b53a53f688ae3785
Ahmed Salah <toushi2009@gmail.com>
2022-03-26 21:44:18 +0200

    fixed migration conflicts for active storage (duplicate)

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_205440) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_153950) do
->>>>>>> Development
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_204612) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,11 +39,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_153950) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,7 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_153950) do
     t.integer "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
