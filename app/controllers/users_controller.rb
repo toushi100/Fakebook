6ae672557c563480d3bdfa191bb92b7b8e98ca8e
AbdelrahmanMohamed <abdelrahman.muhamedd@gmail.com>
2022-03-26 03:27:20 +0200

    Friend request controller added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
   end
+  def send_friend_request
+    friendlist = new Friendlist()
+    friendlists.user_id = current_user
+    friendlists.friend_id = User.find(params[:id])
+    Friendlist.save()
+
+    # Notification for friend request
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get 'users/index'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, controllers: {  sessions: 'users/sessions'  }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+
   match '/users',   to: 'users#index',   via: 'get'
+  match '/users',   to: 'users#send_friend_request',   via: 'get'
   resources :users, :only =>[:show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
   root to: "home#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
