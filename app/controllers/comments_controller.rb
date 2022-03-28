f1ffe117a717362552adbbeeaa3ecd33d618d02a
Ahmed Salah <toushi2009@gmail.com>
2022-03-28 15:27:34 +0200

    fixed create method and destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,24 @@
 class CommentsController < ApplicationController
+  @comments = Comment.all
 
-     @comments = Comment.all
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new()
+    @comment.content = params["comment"]["content"]
+    @comment.user_id = current_user.id
+    @comment.save()
+    redirect_to request.referrer
+  end
 
+  def destroy_comment
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referrer
+  end
 
-    def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment].permit(:content, :post_id, :user_id))
-        @comment = current_user.comments.new(comment_params)
-        redirect_to post_path(params[:post_id])
-    end
+  private
 
-    def destroy_comment
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.find(params[:id])
-        @comment.destroy
-        redirect_to post_path(@post)
-    end
-
-    private
-
-    def comment_params
-        params.require(:comment).permit(:content, :post_id, :user_id)
-    end
-
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
 end
