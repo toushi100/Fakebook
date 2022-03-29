4a58598f387eb8ae7e665fb4ed84a98b55fc960f
Mady <ahmed.mady99@gmail.com>
2022-03-29 14:15:52 +0200

    removed comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,16 +18,7 @@ class CommentsController < ApplicationController
     redirect_to request.referrer
   end
   
-#   def current_user?(user)
-#     user == current_user
-#   end
-
-#   def current_user
-#     if(user_id = session[:user_id])
-#       @current_user ||= User.find_by(id: user_id)
-#     end
-#   end
-
+  
   def correct_user?
     @comment = Comment.find_by(id: params[:id])
     # unless current_user?(@comment.user)
