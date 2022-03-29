9c00afbac2ba6b277bf981bfd77232dcd67e022e
Ahmed Salah <toushi2009@gmail.com>
2022-03-29 23:52:29 +0200

    added a change to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
                  :length => { :minimum => 10, :maximum => 15 }
 
 
+                 
   private
   def add_default_avatar
     unless profile_picture.attached?
