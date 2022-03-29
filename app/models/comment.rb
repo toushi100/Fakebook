642d41c9fe415bc966559a6ecb904b666cf7e788
Mady <ahmed.mady99@gmail.com>
2022-03-29 13:41:21 +0200

    edited comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,20 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  # def current_user?(user)
-  #   user == current_user
-  # end
-
-  # def current_user
-  #   if(user_id = session[:user_id])
-  #     @current_user ||= User.find_by(id: user_id)
-  #   end
-  # end
-
-  # def correct_user
-  #   @comment = Comment.find_by(id: params[:id])
-  #   unless current_user?(@comment.user)
-  #       redirect_to request.referrer
-  #   end
-  # end
+  validates :content, presence: true
 end
