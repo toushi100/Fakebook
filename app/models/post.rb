40799ad11304ad8014b1d04f274b785495659daf
Mady <ahmed.mady99@gmail.com>
2022-03-29 11:35:31 +0200

    add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many_attached :image
   has_many :comments, dependent: :destroy
   belongs_to :user
+
+  validates :text, presence: true
+  validates :image, presence: true
+  
 end
