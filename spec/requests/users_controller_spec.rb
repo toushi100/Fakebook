a5e5ca848f7d3c48d6022559792740920cf31ce2
Ahmed Salah <toushi2009@gmail.com>
2022-04-01 19:21:57 +0200

    added testcases for block friend and unblock friend

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -9,13 +9,7 @@ RSpec.describe "UsersControllers", type: :request do
     Ali = User.create!(user_name: "Ali", email: "Ali@gmail.com",
                        phone_number: 9038456748, profile_picture: @file,
                        password: "123456", password_confirmation: "123456")
-
-                       sign_in Ahmed
-    post send_friend_request_url(Ali)
-    sign_out Ahmed
-    sign_in Ali
-    post accept_friend_request_url
-
+    sign_in Ahmed
   end
   describe "GET /show" do
     it "renders a successful response for show user (profile)" do
@@ -31,10 +25,9 @@ RSpec.describe "UsersControllers", type: :request do
   describe "GET /index" do
     it "renders a successful response when user is not signed in" do
       get users_index_url
-
-    
-
- 
+      expect(response).to_not be_successful
+      expect(response).to redirect_to(new_user_session_url)
+    end
 
     it "renders a successful response when user is signed in" do
       sign_in Ahmed
@@ -46,53 +39,77 @@ RSpec.describe "UsersControllers", type: :request do
     it "renders a successful response when user is signed in" do
       sign_in Ahmed
       expect do
-      post send_friend_request_url(Ali), params: {"id" => Ali.id}
-      end.to change(Friendlist,:count).by(1)
+        post send_friend_request_url(Ali), params: { "id" => Ali.id }
+      end.to change(Friendlist, :count).by(1)
     end
 
     it "renders a redirect to sign in response when user is not signed in" do
-      post send_friend_request_url(Ali), params: {"id" => Ali.id}
+      post send_friend_request_url(Ali), params: { "id" => Ali.id }
       expect(response).to redirect(new_user_session_url)
     end
-
   end
 
   describe "DELETE users/remove_friend_request/:id" do
     it "renders successful " do
       sign_in Ahmed
-      post send_friend_request_url(Ali), params: {"id" => Ali.id}
+      post send_friend_request_url(Ali), params: { "id" => Ali.id }
       sign_in Ali
       post accept_friend_request_url
       expect do
-      delete remove_friend_request_url(Ali), params: {"id" => Ali.id}
-      end.to change(Friendlist,:count).by(1)
+        delete remove_friend_request_url(Ali), params: { "id" => Ali.id }
+      end.to change(Friendlist, :count).by(1)
     end
     it "redirects user to sign in page if user is not signed in " do
       sign_out Ahmed
       sign_out Ali
       expect do
-      delete remove_friend_request_url(Ali), params: {"id" => Ali.id}
+        delete remove_friend_request_url(Ali), params: { "id" => Ali.id }
       end.to redirect_to(new_user_session_url)
     end
 
     it "friend should be able to remove friend request " do
       sign_in Ali
-      post send_friend_request_url(Ahmed), params: {"id" => Ali.id}
+      post send_friend_request_url(Ahmed), params: { "id" => Ali.id }
       expect do
-      delete remove_friend_request_url(Ali), params: {"id" => Ali.id}
-      end.to change(Friendlist,:count).by(1)
+        delete remove_friend_request_url(Ali), params: { "id" => Ali.id }
+      end.to change(Friendlist, :count).by(1)
     end
   end
 
   describe "remove_friend DELETE /users/remove_friend/:id" do
     it "renders successful " do
       sign_in Ahmed
-      post send_friend_request_url(Ali), params: {"id" => Ali.id}
+      post send_friend_request_url(Ali), params: { "id" => Ali.id }
       sign_in Ali
       post accept_friend_request_url
       expect do
-      delete remove_friend_url(Ali), params: {"id" => Ali.id}
-      end.to change(Friendlist,:count).by(1)
+        delete remove_friend_url(Ali), params: { "id" => Ali.id }
+      end.to change(Friendlist, :count).by(1)
     end
   end
+
+  describe "POST   /users/block_friend/:id" do
+      it "should block friend successfully " do
+        sign_in Ahmed
+        post send_friend_request_url(Ali)
+        sign_out Ahmed
+        sign_in Ali
+        post accept_friend_request_url
+        post block_friend_url(Ahmed)
+        expect(response).to be_successful
+    end
+  end
+
+  describe "DELETE /users/un_block_friend/:id" do
+    it "should UN-block friend successfully " do
+      sign_in Ahmed
+      post send_friend_request_url(Ali)
+      sign_out Ahmed
+      sign_in Ali
+      post accept_friend_request_url
+      post block_friend_url(Ahmed)
+      delete un_block_friend_url(Ahmed)
+      expect(response).to be_successful
+  end
+end
 end
