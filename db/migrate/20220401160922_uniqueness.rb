1d5e0e8834fd23140c57a5dc456e023fd427819e
Ahmed Salah <toushi2009@gmail.com>
2022-04-03 00:57:03 +0200

    removed Uniqueness migration it alreaady exsists in likes model

--- db/migrate/20220401160922_uniqueness.rb
+++
@@ -1,5 +0,0 @@
-class Uniqueness < ActiveRecord::Migration[7.0]
-  def change
-    add_index :likes, [:user_id, :post_id], unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -79,16 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_185728) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "loves", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_loves_on_post_id"
-    t.index ["user_id", "post_id"], name: "index_loves_on_user_id_and_post_id", unique: true
-    t.index ["user_id"], name: "index_loves_on_user_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
@@ -141,8 +131,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_185728) do
   add_foreign_key "hearts", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "loves", "posts"
-  add_foreign_key "loves", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "sads", "posts"
   add_foreign_key "sads", "users"
