3e98359eeefab088a659b34edb7b6907c75bc4ca
Ahmed Salah <toushi2009@gmail.com>
2022-04-03 05:15:53 +0200

    added dependant to reacts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   has_many_attached :image
   has_many :comments, dependent: :destroy
-  has_many :likes
-  has_many :angries
-  has_many :hearts
-  has_many :sads
-  has_many :wows
+  has_many :likes, dependent: :destroy
+  has_many :angries, dependent: :destroy
+  has_many :hearts, dependent: :destroy
+  has_many :sads, dependent: :destroy
+  has_many :wows, dependent: :destroy
   belongs_to :user
 
   validates :text, presence: true
