4c03ba847385d4d162bb5543bf24f2d8a7da1cf8
Ahmed Salah <toushi2009@gmail.com>
2022-04-03 10:00:33 +0200

    added validation for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, length: { minimum: 1 , message: "cannot be less than 1 character" }
 end
