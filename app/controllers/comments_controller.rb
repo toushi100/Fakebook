cb7da0906cf5c37994261f6a3551e28272e8089a
Ahmed Salah <toushi2009@gmail.com>
2022-04-03 10:00:00 +0200

    Added redirect to non signed in users to sign in page on create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,12 +4,16 @@ class CommentsController < ApplicationController
   @comments = Comment.all
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.new()
-    @comment.content = params["comment"]["content"]
-    @comment.user_id = current_user.id
-    @comment.save()
-    redirect_to post_url(@post)
+    if current_user != nil
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.new()
+      @comment.content = params["comment"]["content"]
+      @comment.user_id = current_user.id
+      @comment.save()
+      redirect_to post_url(@post)
+    else
+      redirect_to new_user_session_url
+    end
   end
 
   def update_comment
@@ -23,14 +27,13 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.destroy
   end
-  
-  
+
   def correct_user?
     @comment = Comment.find_by(id: params[:id])
     # unless current_user?(@comment.user)
     #     redirect_to request.referrer
     # end
-    redirect_to post_url(@comment.post_id), notice: "Cannot delete others comment" unless @comment.user == current_user
+    redirect_to post_url(@comment.post_id), notice: "Cannot delete other users' comments" unless @comment.user == current_user
   end
 
   private
