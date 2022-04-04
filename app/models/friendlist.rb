5b281b5fcc28940cfbd4a4c1cf9159a6e91be2b5
Ahmed Salah <toushi2009@gmail.com>
2022-04-04 02:45:17 +0200

    removed validates_with

--- app/models/friendlist.rb
+++ app/models/friendlist.rb
@@ -1,13 +1,4 @@
-
-# class FrierndlistValidator < ActiveModel::Validator
-#   # def validate(record)
-#   #   if true
-#   #     record.errors.add :base, "This person is evil"
-#   #   end
-#   # end
-# end
 class Friendlist < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-  # validates_with FrierndlistValidator
 end
