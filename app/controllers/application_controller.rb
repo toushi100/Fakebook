e2666ebe0890152100942a92c6d66228d7a567e2
Rawan <rawanalex15@gmail.com>
2022-04-18 01:16:09 +0200

    prohibiting routing to homepage fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
     around_action :switch_locale
     
   protected
